5a78bb67d532eff941d21a81dc81b93cc7060182
Stefan Wolf <wolf@gradle.com>
2017-06-20 09:42:51 +0200

    Test something in integration test

--- application/src/test/java/com/example/application/ApplicationStartupTest.java
+++ application/src/test/java/com/example/application/ApplicationStartupTest.java
@@ -1,17 +1,30 @@
 package com.example.application;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertTrue;
 
 public class ApplicationStartupTest {
 
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
     @Test
     public void applicationShouldStartup() throws IOException {
-        Files.list(Paths.get(System.getenv().get("INTEGRATION_TEST_SAMPLES"))).forEach(path ->
-                Main.main(new String[]{ path.toString() })
+        Stream<Path> integrationTestSamples = Files.list(Paths.get(System.getenv().get("INTEGRATION_TEST_SAMPLES")));
+        integrationTestSamples.forEach(path -> Main.main(new String[]{path.toString()})
         );
+
+        String log = systemOutRule.getLog();
+        assertTrue("Gradle Summit", log.contains("Gradle Summit"));
+        assertTrue("Build cache", log.contains("Build cache"));
     }
 }
